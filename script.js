8a3fdff4b9635a20e7b2139266d1670d5be1f83a
ferdikorkut <ferdikorkut@gmail.com>
2026-06-15 12:48:03 +0200

    Bildirim otomatik kapanma süresini 10 saniyeden 7 saniyeye düşür

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function bildirimGoster(mesaj) {
   clearTimeout(bildirimZamanlayici);
   bildirimZamanlayici = setTimeout(() => {
     bildirimKutu.classList.remove("goster");
-  }, 10000);
+  }, 7000);
 }
 
 // "sepet" dizisindeki verilere göre sayfanın sepet bölümünü
